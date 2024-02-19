9741561228cfe698d6f445e4343e8a102f90f5e4
ray <sandasan619@yahoo.co.jp>
2024-02-19 15:41:34 +0900

    完全体

--- sairai/src/main/java/jp/co/brightstar/dvd/controller/DVDController.java
+++ sairai/src/main/java/jp/co/brightstar/dvd/controller/DVDController.java
@@ -1,11 +1,14 @@
 package jp.co.brightstar.dvd.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import jp.co.brightstar.dvd.model.Condi;
@@ -67,5 +70,10 @@ public class DVDController {
 
 		return mav;
 	}
-
+@PostMapping("/delete")
+@ResponseBody
+public Map<String , Object>delete(DVD dvd){
+	Map<String ,Object>map = new HashMap<>();
+	return map;
+}
 }
